963eb58eb2a4a9a00e181234c1b7db1cf232db5c
Agnieszka <aga_bylicka@poczta.onet.pl>
2021-05-30 15:23:27 +0200

    init of function

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def validate_branch_name(name):
+
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    validate_branch_name('PyCharm')
+
